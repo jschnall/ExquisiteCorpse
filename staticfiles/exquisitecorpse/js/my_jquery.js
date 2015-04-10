5d9ed43df1a02659cf767d80edb832742ed37a93
Joel Schnall <jschnall@gmail.com>
2015-04-10 01:10:50 -0700

    working on toggling likes/favorites

--- staticfiles/exquisitecorpse/js/my_jquery.js
+++ staticfiles/exquisitecorpse/js/my_jquery.js
@@ -4,12 +4,3 @@ jQuery(document).ready(function($) {
         window.document.location = $(this).data("href");
     });
 });
-
-// Reload single page element by id
-<script type="text/javascript">
-    function reload(elementId){
-            var container = document.getElementById(elementId);
-            var content = container.innerHTML;
-            container.innerHTML= content;
-        }
-</script>
